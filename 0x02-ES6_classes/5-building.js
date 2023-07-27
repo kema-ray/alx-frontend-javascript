929215e1843c7d292a565a2e21f04df4ade02fc2
kema-ray <rachelkemuma99@gmail.com>
2023-07-27 10:02:34 +0300

    A building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+    constructor(sqft) {
+      this._sqft = sqft;
+    }
+  
+    // Getter for sqft
+    get sqft() {
+      return this._sqft;
+    }
+  
+    // Abstract method
+    evacuationWarningMessage() {
+      if (typeof this.evacuationWarningMessage === 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }  
